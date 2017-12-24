7bcb4294d663922c5d15ce504d332b107fc81eb2
slawek367 <slawomir.bando@gmail.com>
2017-12-24 01:26:09 +0000

    Added orientation function

--- led_display/screen_1.py
+++ led_display/screen_1.py
@@ -24,9 +24,9 @@ print("Object screen1 created.")
 #There you could see all methods from max7219.led.matrix class
 #https://max7219.readthedocs.io/en/0.2.3/max7219.html#module-max7219.led
 
-invert(value, redraw=True) TODO
+invert(value, redraw=True) DONE
 letter(deviceId, asciiCode, font=None, redraw=True) DONE
-orientation(angle, redraw=True) TODO
+orientation(angle, redraw=True) DONE
 pixel(x, y, value, redraw=True) TODO
 scroll_down(redraw=True) TODO
 scroll_up(redraw=True) TODO
@@ -36,12 +36,36 @@ show_message(text, font=None, delay=0.05, always_scroll=False) TODO
 def test_letters():
 	for letter in string.ascii_lowercase:
 		screen1.letter(0, ord(letter))
-		time.sleep(0.2)
+		time.sleep(0.1)
 		
 	for letter in string.ascii_uppercase:
 		screen1.letter(0, ord(letter))
-		time.sleep(0.2)
+		time.sleep(0.1)
+
+def test_invert(screen):
+	is_inverted = False
+	
+	for x in range(8):
+		is_inverted = not is_inverted
+		screen.invert(is_inverted)
+		time.sleep(0.5)
+
+def test_orient(screen):
+	
+	degrees = (0, 90, 180, 270)
+	
+	for degree in degrees:
+		print("Set orientation to: " + str(degree) + " degrees")
+		screen.orientation(degree)
+		time.sleep(1)
+	
+	screen.orientation(0)
 
 wait(3.0, "Testing of letter functions will start in: ")
 test_letters()
 
+wait(3.0, "Testing of invert function will start in: ")
+test_invert(screen1)
+
+wait(3.0, "Testing of orientation function will start in: ")
+test_orient(screen1)
