22ff060ff17e7e4e1dbc44fe287c69afa28056b7
slawek367 <slawomir.bando@gmail.com>
2018-02-28 17:15:01 +0000

    Adding sensors checking

--- PiFollower/test.py
+++ PiFollower/test.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import os
+import sys
 #GPIO.setmode(GPIO.BCM) #GPIO pins
 GPIO.setmode(GPIO.BOARD) #by pin number
-
 GPIO.setwarnings(False)
 
 p = {
@@ -19,7 +20,29 @@ p = {
     "SWITCH_right":36
 }
 
-czujniki = [7, 11, 15, 13, 21, 19, 36, 23, 31, 29, 33, 35] #od lewej do prawej
+
+def set_sensors(sensors):
+    GPIO.setup(sensors, GPIO.IN)
+
+def check_sensors_state(sensors):
+    sensors_state = []
+    for sensor in sensors:
+        if GPIO.input(sensor):
+            sensors_state.append("- ")
+        else:
+            sensors_state.append("+ ")
+    return sensors_state
+
+sensors = [7, 11, 15, 13, 21, 19, 21, 23, 31, 29, 33, 35] #od lewej do prawej
+set_sensors(sensors)
+
+while True:
+    for x in check_sensors_state(sensors):
+        sys.stdout.write(x)
+    print("\n")
+    sleep(0.2)
+
+'''
 GPIO.setup(p["AIN_1"], GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(p["AIN_2"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["BIN_1"], GPIO.OUT, initial=GPIO.LOW)
@@ -27,4 +50,5 @@ GPIO.setup(p["BIN_2"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["PWM_a"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["PWM_b"], GPIO.OUT, initial=GPIO.HIGH)
 sleep(3)
-GPIO.clean()
+GPIO.cleanup()
+'''
