23744c1daa62d6aac929251d5fa6eef169a0bcf3
slawek367 <slawomir.bando@gmail.com>
2018-03-07 16:23:42 +0000

    Brightness test

--- oled_sh1106/test.py
+++ oled_sh1106/test.py
@@ -9,5 +9,14 @@ device = sh1106(serial, rotate=0)
 # Box and text rendered in portrait mode
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((10, 40), "Hello World", fill="white")
-sleep(5)
+
+while True:
+    for x in range(0,255,5):
+        device.contrast(x)
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, outline="white", fill="black")
+            draw.text((3, 3), "Brightness:" + str(x/255*100)[0:5] + "%", fill="white")
+        sleep(0.05)
+    with canvas(device) as draw:
+            draw.text((10, 40), "Test done", fill="white")
+    sleep(3)
