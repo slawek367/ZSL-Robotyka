d4588fd065e69abc747471f125ffbd98e9c48195
slawek367 <slawomir.bando@gmail.com>
2018-03-21 00:33:01 +0000

    requests ecample

--- json_api/examples_requests.py
+++ json_api/examples_requests.py
@@ -1,3 +1,16 @@
 import json
 import requests
 import sys
+
+#We need to specify what data on what data we working
+headers = {'Content-Type': 'application/json; charset=utf-8'}
+
+# RUN GET REQUEST to URL
+url = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
+response = requests.get(url, headers)
+response_code = response.status_code
+response_json = json.loads(response.content.decode('utf-8'))
+
+print("Status code: " + str(response_code))
+print("First element from json response: \n" + str(response_json[0]))
+
