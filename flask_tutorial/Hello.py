30531d81c8c49bde3afc421b2f0583add302d544
Slawomir Bando <slawomir.bando@ericsson.com>
2018-04-11 17:51:56 +0200

    Added flask tutorial

---
+++ flask_tutorial/Hello.py
@@ -0,0 +1,31 @@
+#Tutorial: https://www.tutorialspoint.com/flask/flask_application.htm
+
+'''
+{% ... %} for Statements
+{{ ... }} for Expressions to print to the template output
+{# ... #} for Comments not included in the template output
+# ... ## for Line Statements
+'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return "Hello World"
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name = name)
+
+@app.route('/index')
+def index():
+    #return '<html><body><h1>Hello World</h1></body></html>'
+    return render_template('index.html')
+
+@app.route('/sensors')
+def get_sensors():
+    sensor_list = {"sensor1": 50, "sensor2": 60, "sensor3": 65}
+    return render_template('sensors.html', sensors = sensor_list)
+
+if __name__ == '__main__':
+   app.run(port=5011, debug=True)
